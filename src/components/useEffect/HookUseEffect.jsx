3ead2e8b7fe018eb099ccf9b5afe4b8e048dd525
DiegoDoria <retrexretrex@hotmail.com>
2024-10-31 20:56:35 -0500

    useEffect treminado

--- src/components/useEffect/HookUseEffect.jsx
+++ src/components/useEffect/HookUseEffect.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './HookUseEffect.css'
+import axios from 'axios'
 
 export default function HookUseEffect() {
+
+  const [personajes, setPersonajes] = useState([])
+
+  useEffect(() => {
+    llamarPersonajes();
+  },[]);
+
+
+  async function llamarPersonajes(){
+    const getPersonajes = await axios.get('https://dragonball-api.com/api/characters')
+    setPersonajes(getPersonajes.data.items)
+  }
+  
+  
+  
+
   return (
     <div className='effectcontainer'>
-      HookUseEfect
+      {/* HookUseEfect */}
+
+      <table>
+        <thead>
+          <tr>
+            <td>Nombre</td>
+            <td>Ki</td>
+            <td>Raza</td>
+            <td>Generp</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            personajes.map((item, index) => (
+              <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.ki}</td>
+                <td>{item.race}</td>
+                <td>{item.gender}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+
     </div>
   )
 }
